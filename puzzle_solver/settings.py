0a225cb430ff9c27c4d71768cbc4fc16113a422a
zainic <ihanik.ad@gmail.com>
2025-02-06 12:22:36 +0600

    deploy in vercel

--- puzzle_solver/settings.py
+++ puzzle_solver/settings.py
@@ -115,6 +115,7 @@ USE_I18N = True
 USE_TZ = True
 CORS_ALLOWED_ORIGINS = [
     "https://eight-puzzle-solver.vercel.app",
+    "http://localhost:5173",
 ]   
 CORS_ALLOW_HEADERS = [
     "authorization",
