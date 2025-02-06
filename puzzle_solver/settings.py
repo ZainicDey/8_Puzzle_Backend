e1a640405027b50da153f0562f27db7cdb5160ae
zainic <ihanik.ad@gmail.com>
2025-02-06 11:34:45 +0600

    Check again

--- puzzle_solver/settings.py
+++ puzzle_solver/settings.py
@@ -113,7 +113,9 @@ TIME_ZONE = 'UTC'
 USE_I18N = True
 
 USE_TZ = True
-CORS_ALLOW_ALL_ORIGINS = True
+CORS_ALLOWED_ORIGINS = [
+    "https://eight-puzzle-solver.vercel.app",
+]   
 CORS_ALLOW_HEADERS = [
     "authorization",
     "content-type",
